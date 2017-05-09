e52c853deb26d8bd171350d09bb6357afa60b26b
Ahmad Atef <ahmad.atef@smartech.my>
2017-05-09 23:56:28 +0200

    Added Car Cell. Added OpenSans Font.

--- AutoScout24 Tech_Challenge/Controllers/CarsListViewController.swift
+++ AutoScout24 Tech_Challenge/Controllers/CarsListViewController.swift
@@ -10,10 +10,44 @@ import UIKit
 
 class CarsListViewController: UIViewController {
 
+    //MARK: - IBOutlet -
+    @IBOutlet weak var tableView: UITableView!
+    
+    //MARK: - Properties -
+    var dataSource : [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func addPullToRefresh() {
+        
+    }
 }
 
+//MARK: - TableView Delegates -
+
+extension CarsListViewController : UITableViewDelegate, UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: CarCell.Identifier) as? CarCell{
+            return cell
+        }
+        return UITableViewCell()
+    }
+}
+
+
+class CarCell: UITableViewCell {
+    static let Identifier = "CarCell"
+    @IBOutlet weak var milage: UILabel!
+    @IBOutlet weak var manufacturer: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var isFavorited: UISwitch!
+}
---
+++ AutoScout24 Tech_Challenge/View Model/CarListViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  CarListViewModel.swift
+//  AutoScout24 Tech_Challenge
+//
+//  Created by Ahmad Atef on 5/9/17.
+//  Copyright © 2017 Ahmad Atef. All rights reserved.
+//
+
+import Foundation
+
+
+class CarListViewModel {
+    
+}
