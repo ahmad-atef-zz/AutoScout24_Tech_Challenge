7f8bd42564f4cec0d066bb07fac4200a0c07d0d0
Ahmad Atef <ahmad.atef@smartech.my>
2017-05-09 05:59:29 +0200

    Added ApplicationTabbar Controller

---
+++ AutoScout24 Tech_Challenge/Controllers/ApplicationTabbar.swift
@@ -0,0 +1,38 @@
+//
+//  TestingTabbar.swift
+//  AutoScout24 Tech_Challenge
+//
+//  Created by Ahmad Atef on 5/9/17.
+//  Copyright © 2017 Ahmad Atef. All rights reserved.
+//
+
+import UIKit
+
+class ApplicationTabbar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
